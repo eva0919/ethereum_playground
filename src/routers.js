f92792f0509b0814d95155168bdee2118e624fae
Chen <eva091960601@gmail.com>
2018-04-20 17:15:27 +0800

    add proper http response status

--- src/routers.js
+++ src/routers.js
@@ -7,15 +7,31 @@ var utils = require("./utils.js");
 var web3 = new Web3();
 web3 = utils.wrapper(web3);
 web3.setProvider(new web3.providers.HttpProvider("http://127.0.0.1:8545"));
+web3.eth.net
+  .isListening()
+  .then(() => console.log("web3 is connected"))
+  .catch(e => console.log("web3 can't connect to eth, please check it."));
 
 var router = express.Router();
 
-router.get("/node", function(req, res) {
-  web3.admin.nodeInfo().then(function(response) {
-    res.json(response.result);
-  });
+function checkEthMiddle(req, res, next) {
+  web3.eth.net
+    .isListening()
+    .then(() => next())
+    .catch(e => res.status(503).json({ errorMsg: "Some Services gone." }));
+}
+
+router.get("/node", checkEthMiddle, function(req, res) {
+  web3.admin
+    .nodeInfo()
+    .then(function(response) {
+      res.json(response.result);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 });
-router.get("/block/:block_number", function(req, res) {
+router.get("/block/:block_number", checkEthMiddle, function(req, res) {
   var block_info = web3.eth.getBlock(req.params.block_number, function(
     error,
     result
@@ -27,7 +43,7 @@ router.get("/block/:block_number", function(req, res) {
   });
 });
 
-router.get("/transaction/:transation_hash", function(req, res) {
+router.get("/transaction/:transation_hash", checkEthMiddle, function(req, res) {
   var transation_info = web3.eth.getTransaction(
     req.params.transation_hash,
     function(error, result) {
@@ -39,19 +55,19 @@ router.get("/transaction/:transation_hash", function(req, res) {
   );
 });
 
-router.put("/miner", function(req, res) {
+router.put("/miner", checkEthMiddle, function(req, res) {
   web3.miner.start(1).then(function(result) {
-    res.json(result);
+    res.status(201).json(result);
   });
 });
 
-router.delete("/miner", function(req, res) {
+router.delete("/miner", checkEthMiddle, function(req, res) {
   web3.miner.stop().then(function(result) {
-    res.json(result);
+    res.status(202).json(result);
   });
 });
 
-router.post("/transaction", function(req, res) {
+router.post("/transaction", checkEthMiddle, function(req, res) {
   console.log(req.body);
   var params = req.body;
   web3.eth.personal
@@ -70,7 +86,7 @@ router.post("/transaction", function(req, res) {
     .catch(function(error) {
       console.log(error);
     });
-  res.json(req.body);
+  res.status(202).json(req.body);
 });
 
 module.exports = router;
